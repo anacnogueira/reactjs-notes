31f1d0e8f5f39160d701518d3b4d580f1076f2d2
Ana Claudia <anacnogueira@gmail.com>
2024-02-14 15:40:30 -0300

    Finaliza modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface NoteCardProps {
   note: {
@@ -19,8 +21,18 @@ export function NoteCard({ note }: NoteCardProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
-          <div className="flex flex-1"></div>
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-small font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+            <p className="text-small leading-6 text-slate-400">
+              {note.content}
+            </p>
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
